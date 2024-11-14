8329ab1e9bd39a2c94e4ae37adcd7cd777985181
llucasmenezes <llucasmenezes99@gmail.com>
2024-11-14 14:14:24 -0300

    feat: controller criado para Missoes e Ninja

--- src/main/java/dev/java10x/CadastroDeNinjas/controller/MissoesController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/controller/MissoesController.java
@@ -1,4 +1,52 @@
 package dev.java10x.CadastroDeNinjas.controller;
 
+import dev.java10x.CadastroDeNinjas.entities.MissoesModel;
+import dev.java10x.CadastroDeNinjas.repository.MissoesRepository;
+import dev.java10x.CadastroDeNinjas.service.MissoesService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/missoes")
 public class MissoesController {
+
+MissoesService missoesService;
+MissoesRepository missoesRepository;
+
+    @GetMapping("/listar")
+    public ResponseEntity<List<MissoesModel>> getAllMission() {
+    List<MissoesModel> missoes  = missoesService.getAll();
+    return new ResponseEntity<>(missoes, HttpStatus.OK);
+}
+
+    @GetMapping("missao")
+    public ResponseEntity<MissoesModel> getMissionById(@PathVariable Long id){
+        Optional<MissoesModel> missao = Optional.ofNullable(missoesService.findById(id));
+        if(missao.isPresent()){
+            return ResponseEntity.ok(missao.get());
+        }else{
+            return  ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping("salvar")
+    public ResponseEntity<MissoesModel> saveMission(@RequestBody MissoesModel missoesModel){
+        MissoesModel savedMissao = missoesService.saveMissao(missoesModel);
+        return new ResponseEntity<>(savedMissao, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> deleteMissao(@PathVariable  Long id){
+
+        if(missoesRepository.existsById(id)){
+            missoesService.deleteMissaoById(id);
+            return ResponseEntity.noContent().build();
+        } else{
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
--- src/main/java/dev/java10x/CadastroDeNinjas/controller/NinjaController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/controller/NinjaController.java
@@ -1,15 +1,62 @@
 package dev.java10x.CadastroDeNinjas.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import dev.java10x.CadastroDeNinjas.entities.NinjaModel;
+import dev.java10x.CadastroDeNinjas.service.NinjaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
-@RequestMapping
+@RequestMapping("/ninjas")
 public class NinjaController {
+
+    @Autowired
+    NinjaService ninjaService;
 
     @GetMapping("/boasVindas")
     public String boasVindas() {
         return "Essa é a minha primeira mensagem nessa rota";
     }
-}
+
+
+    @GetMapping("/listar")
+    public ResponseEntity<List<NinjaModel>> getAllNinjas() {
+       List<NinjaModel> ninjas = ninjaService.findAll();
+       return new ResponseEntity<>(ninjas, HttpStatus.OK);
+    }
+
+    @PostMapping("/criar")
+    public ResponseEntity<NinjaModel> saveNinja(@RequestBody NinjaModel ninjaModel){
+        NinjaModel ninja = ninjaService.createNinja(ninjaModel);
+        return new ResponseEntity<>(ninja, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/listar/{id}")
+    public ResponseEntity<NinjaModel> getNinjaById(@PathVariable  Long id){
+        NinjaModel ninja = ninjaService.findById(id);
+        if(ninja.getId() == null){
+           return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+       return new ResponseEntity<>(ninja, HttpStatus.OK);
+    }
+
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteNinja(@PathVariable Long id) {
+        boolean isDeleted = ninjaService.deleteNinjaById(id);
+        if (isDeleted) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+
+
+ }
+

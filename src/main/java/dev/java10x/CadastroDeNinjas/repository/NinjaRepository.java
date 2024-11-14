6f280e1ce83410ad43f511d67087158e95356b5d
llucasmenezes <llucasmenezes99@gmail.com>
2024-11-14 14:16:15 -0300

    feat: incluindo o ninjaRepository

--- src/main/java/dev/java10x/CadastroDeNinjas/repository/NinjaRepository.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/repository/NinjaRepository.java
@@ -2,6 +2,8 @@ package dev.java10x.CadastroDeNinjas.repository;
 
 import dev.java10x.CadastroDeNinjas.entities.NinjaModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface NinjaRepository extends JpaRepository<NinjaModel, Long> {
 }

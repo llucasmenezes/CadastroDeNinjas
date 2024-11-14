44db50ed6ff91915ba73b6f3b523b0ddf1a6a82d
llucasmenezes <llucasmenezes99@gmail.com>
2024-11-14 14:13:42 -0300

    feat: service criado para Missoes e Ninja

--- src/main/java/dev/java10x/CadastroDeNinjas/service/MissoesService.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/service/MissoesService.java
@@ -1,10 +1,32 @@
 package dev.java10x.CadastroDeNinjas.service;
 
+import dev.java10x.CadastroDeNinjas.entities.MissoesModel;
 import dev.java10x.CadastroDeNinjas.repository.MissoesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.Optional;
 
 public class MissoesService {
 
-    MissoesRepository repository;
+    @Autowired
+    private MissoesRepository missoesRepository;
 
 
+    public List<MissoesModel> getAll(){
+        return missoesRepository.findAll();
+    }
+
+    public MissoesModel findById(Long id) {
+        Optional<MissoesModel> missoes = missoesRepository.findById(id);
+        return missoes.orElseThrow(() -> new RuntimeException("Missão não encontrada!"));
+    }
+
+    public MissoesModel saveMissao(MissoesModel missoesModel){
+        return missoesRepository.save(missoesModel);
+    }
+
+    public void deleteMissaoById(Long id){
+         missoesRepository.deleteById(id);
+    }
 }
--- src/main/java/dev/java10x/CadastroDeNinjas/service/NinjaService.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/service/NinjaService.java
@@ -1,9 +1,67 @@
 package dev.java10x.CadastroDeNinjas.service;
 
+import dev.java10x.CadastroDeNinjas.entities.MissoesModel;
+import dev.java10x.CadastroDeNinjas.entities.NinjaModel;
+import dev.java10x.CadastroDeNinjas.repository.MissoesRepository;
 import dev.java10x.CadastroDeNinjas.repository.NinjaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class NinjaService {
 
+    @Autowired
     NinjaRepository repository;
 
+    @Autowired
+    private MissoesRepository missoesRepository;
+
+    public NinjaModel createNinja(NinjaModel ninjaModel) {
+        // Salvar a missão se ela ainda não estiver salva
+        if (ninjaModel.getMissoes() != null && ninjaModel.getMissoes().getId() == null) {
+            MissoesModel savedMissao = missoesRepository.save(ninjaModel.getMissoes());
+            ninjaModel.setMissoes(savedMissao); // Atualiza a missão com a entidade salva
+        }
+        return repository.save(ninjaModel); // Salva o ninja no banco de dados
+    }
+
+    public NinjaModel findById(Long id){
+        Optional<NinjaModel> ninjaPorId = repository.findById(id);
+        return ninjaPorId.orElse(null);
+    }
+
+    public List<NinjaModel> findAll() {
+        return repository.findAll();
+    }
+
+    public boolean deleteNinjaById(Long id) {
+        repository.deleteById(id);
+        return false;
+   }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

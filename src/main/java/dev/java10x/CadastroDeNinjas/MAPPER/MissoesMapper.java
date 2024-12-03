943ff9c9170597055f2558cc73ff27380c6f4016
llucasmenezes <llucasmenezes99@gmail.com>
2024-12-03 19:47:15 -0300

    chore: refatorando os mappers, tirando eles da atualizacao manual e colocando eles automaticos.

--- src/main/java/dev/java10x/CadastroDeNinjas/MAPPER/MissoesMapper.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/MAPPER/MissoesMapper.java
@@ -2,26 +2,14 @@ package dev.java10x.CadastroDeNinjas.MAPPER;
 
 import dev.java10x.CadastroDeNinjas.DTO.MissoesDTO;
 import dev.java10x.CadastroDeNinjas.entities.MissoesModel;
+import org.mapstruct.Mapper;
 import org.springframework.stereotype.Component;
 
-@Component
-public class MissoesMapper {
+@Mapper(componentModel = "spring" )
+public interface MissoesMapper {
 
-public MissoesModel map(MissoesDTO missoesDTO){
-    MissoesModel missoesModel = new MissoesModel();
-    missoesModel.setId(missoesDTO.getId());
-    missoesModel.setNome(missoesDTO.getNome());
-    missoesModel.setNinjas(missoesDTO.getNinjas());
-    missoesModel.setDificuldade(missoesDTO.getDificuldade());
-    return missoesModel;
-}
+  MissoesModel map(MissoesDTO missoesDTO);
 
-public MissoesDTO map(MissoesModel missoesModel){
-    MissoesDTO missoesDTO = new MissoesDTO();
-    missoesDTO.setId(missoesModel.getId());
-    missoesDTO.setNome(missoesModel.getNome());
-    missoesDTO.setNinjas(missoesModel.getNinjas());
-    missoesDTO.setDificuldade(missoesModel.getDificuldade());
-    return missoesDTO;
-}
+  MissoesDTO map(MissoesModel missoesModel);
+
 }
--- src/main/java/dev/java10x/CadastroDeNinjas/MAPPER/NinjaMapper.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/MAPPER/NinjaMapper.java
@@ -2,30 +2,12 @@ package dev.java10x.CadastroDeNinjas.MAPPER;
 
 import dev.java10x.CadastroDeNinjas.DTO.NinjaDTO;
 import dev.java10x.CadastroDeNinjas.entities.NinjaModel;
+import org.mapstruct.Mapper;
 
-public class NinjaMapper {
+@Mapper(componentModel = "spring" )
+public interface NinjaMapper {
 
-    public NinjaModel map(NinjaDTO ninjaDTO){
-        NinjaModel ninjaModel = new NinjaModel();
-        ninjaModel.setId(ninjaDTO.getId());
-        ninjaModel.setName(ninjaDTO.getName());
-        ninjaModel.setAge(ninjaDTO.getAge());
-        ninjaModel.setEmail(ninjaDTO.getEmail());
-        ninjaModel.setImgUrl(ninjaDTO.getImgUrl());
-        ninjaModel.setRank(ninjaDTO.getRank());
-        ninjaModel.setMissoes(ninjaDTO.getMissoes());
-        return ninjaModel;
-    }
+     NinjaModel map(NinjaDTO ninjaDTO);
+     NinjaDTO map(NinjaModel ninjaModel);
 
-    public NinjaDTO map (NinjaModel ninjaModel){
-        NinjaDTO ninjaDTO = new NinjaDTO();
-        ninjaDTO.setId(ninjaModel.getId());
-        ninjaDTO.setName(ninjaModel.getName());
-        ninjaDTO.setAge(ninjaModel.getAge());
-        ninjaDTO.setEmail(ninjaModel.getEmail());
-        ninjaDTO.setImgUrl(ninjaModel.getImgUrl());
-        ninjaDTO.setRank(ninjaModel.getRank());
-        ninjaDTO.setMissoes(ninjaModel.getMissoes());
-        return ninjaDTO;
-    }
 }
